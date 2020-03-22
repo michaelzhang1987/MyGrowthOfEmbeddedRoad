391431261b94a60684f97bcfaf101b31fedd1a55
geek_michael <zbxx1987@gmail.com>
2020-03-22 22:58:40 +0800

    添加自己的文档（第一章），make html成功

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -26,12 +26,23 @@ release = 'v0.1'
 
 
 # -- General configuration ---------------------------------------------------
+# The master toctree document.
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
 ]
+
+from recommonmark.parser import CommonMarkParser
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+source_suffix = ['.rst', '.rest' , '.md']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -54,7 +65,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- Documentation/index.rst
+++ Documentation/index.rst
@@ -1,15 +1,16 @@
-.. 从入职到架构师-我的嵌入式软件成长之路 documentation master file, created by
-   sphinx-quickstart on Sun Mar 22 22:42:08 2020.
+.. 从入职到架构师——我的嵌入式软件成长之路 documentation master file, created by
+   sphinx-quickstart on Sun Mar 22 21:10:06 2020.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to 从入职到架构师-我的嵌入式软件成长之路's documentation!
-=================================================================
+Welcome to 从入职到架构师——我的嵌入式软件成长之路's documentation!
+==================================================================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
-
+   
+   10-wo-de-qian-ru-shi-cheng-zhang-zhi-lu/11-zi-wo-jian-shao
 
 
 Indices and tables
